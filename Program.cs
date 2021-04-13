707cb37a1185fbfaca6ec5057b6cf9411de8f2a8
Andrew Moore <mooreandrew015@gmail.com>
2021-04-13 15:09:15 -0400

    coverted string into numbers and added math

--- Program.cs
+++ Program.cs
@@ -19,6 +19,26 @@ namespace VariablesCS
 
             Console.WriteLine($"It is nice to meet you {userName}");
 
+            Console.Write("a random number ");
+            var firstNumberAsString = Console.ReadLine();
+            Console.Write("another random number ");
+            var secondNumberAsString = Console.ReadLine();
+
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+
+            var sum = firstOperand + secondOperand;
+            var difference = firstOperand - secondOperand;
+            var product = firstOperand * secondOperand;
+            var quotient = firstOperand / secondOperand;
+            var remainder = firstOperand % secondOperand;
+
+            Console.WriteLine("The sum of your numbers are" + sum);
+            Console.WriteLine("The difference of your numbers are" + difference);
+            Console.WriteLine("The product of your numbers are" + product);
+            Console.WriteLine("The quotient of your numbers are" + quotient);
+            Console.WriteLine("The remainder of your numbers are" + remainder);
+
 
 
 
