891709da5eb52e421de8879c7fe49b552c8472f5
Andrew Moore <mooreandrew015@gmail.com>
2021-04-13 16:40:37 -0400

    added an if statement and comments

--- Program.cs
+++ Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 
+
 namespace VariablesCS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // none changing variables
             var numberOfCupsOfCoffee = 0;
-
             var fullName = "Andrew Moore";
-
             var today = DateTime.Now;
-
             Console.WriteLine($"The number of cups of coffee i drink is {numberOfCupsOfCoffee}, my name is {fullName}, the date time is {today} ");
-
+            //  added some logic if the name is Alice
             Console.Write("What is your name? ");
             var userName = Console.ReadLine();
-
-            Console.WriteLine($"It is nice to meet you {userName}");
-
+            if (userName == "Alice")
+            {
+                Console.WriteLine("Lets go get a beer");
+                Console.WriteLine("Cheers");
+                // added because i thought this was pretty cool! 
+                //    |
+                Environment.Exit(-1);
+            }
+            else
+            {
+                Console.WriteLine($"It is nice to meet you {userName}");
+            }
+            //  numbers in the console
             Console.Write("a random number ");
             var firstNumberAsString = Console.ReadLine();
             Console.Write("another random number ");
@@ -26,18 +35,21 @@ namespace VariablesCS
 
             var firstOperand = double.Parse(firstNumberAsString);
             var secondOperand = double.Parse(secondNumberAsString);
-
+            // basic math
             var sum = firstOperand + secondOperand;
             var difference = firstOperand - secondOperand;
             var product = firstOperand * secondOperand;
             var quotient = firstOperand / secondOperand;
             var remainder = firstOperand % secondOperand;
+            // basic math totals
+            Console.WriteLine("The sum of your numbers are " + sum);
+            Console.WriteLine("The difference of your numbers are " + difference);
+            Console.WriteLine("The product of your numbers are " + product);
+            Console.WriteLine("The quotient of your numbers are " + quotient);
+            Console.WriteLine("The remainder of your numbers are " + remainder);
 
-            Console.WriteLine("The sum of your numbers are" + sum);
-            Console.WriteLine("The difference of your numbers are" + difference);
-            Console.WriteLine("The product of your numbers are" + product);
-            Console.WriteLine("The quotient of your numbers are" + quotient);
-            Console.WriteLine("The remainder of your numbers are" + remainder);
+
+
 
         }
     }
