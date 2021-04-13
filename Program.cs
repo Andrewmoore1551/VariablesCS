de60fc41f8acc519634f1f807e23612c7fb133ca
Andrew Moore <mooreandrew015@gmail.com>
2021-04-13 13:16:30 -0400

    added variables, magic key to output on one line

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var numberOfCupsOfCoffee = 0;
+
+            var fullName = "Andrew Moore";
+
+            var today = DateTime.Now;
+
+            Console.WriteLine($"The number of cups of coffee i drink is {numberOfCupsOfCoffee}, my name is {fullName}, the date time is {today} ");
+        }
     }
-  }
 }
